a3a46b6d24d46b2519fb20c24ffa64e8d418f4a9
ffchn <filipe.franchini@live.com>
2024-05-16 20:12:09 -0700

    Fixed missing children type props

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -1,4 +1,6 @@
-export default function PageWrapper({ children }) {
+import { PropsWithChildren } from "react"
+
+export default function PageWrapper({ children }: PropsWithChildren) {
   return (
     <>
       <div className="flex flex-col max-w-[80%] mx-auto py-[60px] prose">
